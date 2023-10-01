fcfdcb66bc3db2e90c63fc411c07ab3a24f01bbd
vijay <vijaykv2228@gmail.com>
2023-10-01 23:35:50 +0530

    Last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
-import App from './app';
+import App from './App';
 import './index.css';
 import Profile from './components/Profile';
 
